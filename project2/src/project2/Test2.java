5a4eabcfc965ecdb5a488e198b15083e9f75e420
shivam7422 <63632045+shivam7422@users.noreply.github.com>
2020-04-14 21:48:57 +0530

    initiated

---
+++ project2/src/project2/Test2.java
@@ -0,0 +1,12 @@
+package project2;
+
+public class Test2 {
+
+public static void main (String args[])
+	
+	{
+		System.out.println(" Welcome to java world");
+		System.out.println(" Welcome to Echo World ");
+		System.out.println(" yo yo " );
+	}
+}
