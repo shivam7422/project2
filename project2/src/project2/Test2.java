eefee231ad277d91fa4efa2be82d85a4032ab1ee
shivam7422 <63632045+shivam7422@users.noreply.github.com>
2020-04-14 22:04:18 +0530

    initiate

--- project2/src/project2/Test2.java
+++ project2/src/project2/Test2.java
@@ -8,5 +8,7 @@ public static void main (String args[])
 		System.out.println(" Welcome to java world");
 		System.out.println(" Welcome to Echo World ");
 		System.out.println(" yo yo " );
+		System.out.println(" this is the mobile m having");
+		
 	}
 }
