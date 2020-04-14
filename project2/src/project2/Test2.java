0878a80ab8c4b27bcf22ba32c9e738ab7b22d526
shivam7422 <63632045+shivam7422@users.noreply.github.com>
2020-04-14 22:06:44 +0530

    new branch

--- project2/src/project2/Test2.java
+++ project2/src/project2/Test2.java
@@ -9,6 +9,7 @@ public static void main (String args[])
 		System.out.println(" Welcome to Echo World ");
 		System.out.println(" yo yo " );
 		System.out.println(" this is the mobile m having");
+		System.out.println(" water bottle ");
 		
 	}
 }
